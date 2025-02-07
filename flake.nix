2a392ffcc2012ba48c6e79b551f4d3eeffe084c8
Martin Mašić <martin.masic@proton.me>
2025-02-07 23:18:39 +0100

    First commit. Flake with dev shell. Direnv.

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Ultimate sudoku implementation. Maybe.";
+
+  inputs =
+  {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in
+  with pkgs;
+  {
+    # development shell
+    devShells.${system}.default = mkShell {
+      buildInputs =
+      [
+        clang_19
+
+      ];
+
+    };
+
+  };
+
+}
